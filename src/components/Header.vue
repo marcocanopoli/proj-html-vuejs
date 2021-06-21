0422d7da7323e21e0f5c570c8a59a48e027d887a
marcocanopoli <canopoli.marco@gmail.com>
2021-06-21 15:12:20 +0200

    nav done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,25 +2,32 @@
     <header>
         <!-- top bar -->
         <div class="top-bar">
-            <div class="container">
-                <ul class="inline-list">
-                    <li v-for="contact, index in contactsLinks"
-                        :key="index">
+            <ul class="container inline-list">
+                <li v-for="contact, index in contactsLinks"
+                    :key="index">                        
+                    <a :href="contact.url">
                         <i :class="contact.icon"></i>
-                        <a :href="contact.url">{{ contact.text }}</a>
-                    </li>
-                </ul>
-            </div>
+                        {{ contact.text }}
+                    </a>
+                </li>
+            </ul>
         </div>
         <!-- /top bar -->
 
         <!-- menu -->
-        <nav>
-            <div>
-
-            </div>
-            <ul>
-
+        <nav class="container">
+            <a class="logo" href="#">
+                <span>Nex</span>
+                <span>Gen</span>
+            </a>
+            <ul class="inline-list">
+                <li v-for="link, index in headerLinks"
+                    :key="index">
+                    <a :href="link.url">{{ link.text.toUpperCase() }}</a>
+                </li>
+                <li>
+                    <a class="btn btn-solid" href="#">Get in touch</a>
+                </li>
             </ul>
         </nav>
         <!-- /menu -->
@@ -56,10 +63,16 @@ export default {
     @import '../assets/style/mixins.scss';
 
     header {
+        
+        a {
+            text-decoration: none;                        
+        }
+
         .top-bar {
             @include flex-center ('cross');
             height: 50px;
             background-color: $text-dark-col;
+            font-weight: 300;
 
             ul {
                 display: flex;
@@ -80,16 +93,48 @@ export default {
                     }
 
                     i {
-                        margin-right: 10px;
-                    }
-
-                    a {
-                        text-decoration: none;                        
+                        margin-right: 5px;
                     }
                 }
             }
         }
         
+        nav {
+            @include flex-center ('cross');
+            justify-content: space-between;
+            height: 90px;
+
+            *:not(.btn) {
+                color: $text-dark-col;
+                
+            }
+
+            a {
+                font-size: 18px;
+
+                span {
+                    font-size: 20px;
+                    font-weight: 600;
+                    text-transform: uppercase;
+                    letter-spacing: 5px;
+                    margin-right: 5px;
+
+                    &:first-child {
+                        padding: 10px 0 10px 30px;
+                        border-top-left-radius: 25px;
+                        border-bottom-left-radius: 25px;
+                        color: $brand-col;
+                        background-color: $brand-alpha20-col;
+                    }
+                }
+            }
+
+            ul {
+                li {
+                    margin-left: 40px;
+                }
+            }
+        }
     }
     
 </style>
